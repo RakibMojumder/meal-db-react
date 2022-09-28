e79892366c11950ef55ce53f428acf3303d5b2b4
Rakib Ahmed <sujonahmed45a4@gmail.com>
2022-09-28 13:59:43 +0600

    single meal cart animation added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Meal from './components/Meal/Meal';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import React, { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, [])
+
   return (
     <div className="App">
       <Meal></Meal>
